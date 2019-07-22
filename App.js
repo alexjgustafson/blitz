6cf7e0fc13fd71c7dddea458c01509736c2c6eed
Alex Gustafson <alexjgustafson@gmail.com>
2019-07-22 13:05:08 -0400

    initial state

--- App.js
+++ App.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+export default class App extends Component {
+  state = {
+    mode: 'newGame',  // newGame, play, and result
+    timeControl: 300, // 5 minutes in seconds
+    boardPosition: 0, // 0 for left, 1 for right
+    whiteTime: 0,
+    blackTime: 0,
+    activePlayer: -1, // -1 to pause game, 0 for black to decrement, 1 for white to decrement
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+  render(){
+    return(
+        <View style={this.styles.container}>
+        <Text>Hello, world!</Text>
+        </View>
+    )
+  }
+}
