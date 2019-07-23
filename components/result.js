b935677b34e4b53b706a21e96200415537793bff
Alex Gustafson <alexjgustafson@gmail.com>
2019-07-23 16:37:30 -0400

    results prettier

--- components/result.js
+++ components/result.js
@@ -2,25 +2,66 @@ import React from 'react';
 import {Button, StyleSheet, Text, View } from 'react-native';
 
 export default function Result( props ) {
-  const {losingPlayer, backToStart} = props;
+  const {backToStart, losingPlayer} = props.settings;
+
+  const styles = StyleSheet.create({
+    black: {
+      alignItems: 'center',
+      backgroundColor: '#333',
+      flex: 1,
+      justifyContent: 'center',
+      width: '100%',
+    },
+    container: {
+      alignItems: 'center',
+      backgroundColor: '#fff',
+      flex: 1,
+      justifyContent: 'center',
+    },
+    lossText: {
+      color: losingPlayer ? '#333' : '#fff',
+      transform: [{ rotate: losingPlayer ? '180deg' : '0deg' }],   
+    },
+    white: {
+      alignItems: 'center',
+      backgroundColor: '#fff',
+      flex: 1,
+      justifyContent: 'center',
+      width: '100%',
+    },
+    winText: {
+      color: !losingPlayer ? '#333' : '#fff',
+      transform: [{ rotate: !losingPlayer ? '180deg' : '0deg' }],   
+    },
+  });
+
+  function getResult( int ){
+    if ( int == losingPlayer ) {
+      return(
+        <Text style={styles.lossText}>You lost on time.</Text>
+      )
+    }
+    return(
+        <Text style={styles.winText}>You won!</Text>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>{losingPlayer ? 'White' : 'Black'} lost on time.</Text>
-      <Button
-        title='Back To Start'
-        onPress={() => {
-          backToStart();
-        }}>
-      </Button>
+      <View style={styles.white}>
+        {getResult(1)}
+      </View>
+      <View style={styles.black}>
+        {getResult(0)}
+        <Button
+          title='Back To Start'
+          onPress={() => {
+            backToStart();
+          }}>
+        </Button>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
